e208dd362f82208fb1f31747e9a4e40115f16a9c
cvaldrighi <valdrighdev@gmail.com>
2022-06-20 13:02:08 -0300

    initial commit

---
+++ JwtAuthApi/Model/User.cs
@@ -0,0 +1,11 @@
+﻿namespace JwtAuthApi.Model
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
